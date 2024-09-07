4328509c34e968afbd694317fdf8817289556963
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-09-07 17:28:51 +0200

    Texture on Texture with colors + glm

--- resources/shaders/basic.glsl
+++ resources/shaders/basic.glsl
@@ -3,21 +3,29 @@
 
 layout (location = 0) in vec3 positions;
 layout (location = 1) in vec3 colors;
+layout (location = 2) in vec2 texture_coordinates;
+
 out vec3 ourColor;
+out vec2 ourTexCoord;
 
 void main()
 {
     gl_Position = vec4(positions, 1.0);
     ourColor = colors;
+    ourTexCoord = texture_coordinates;
 }
 
 //shader fragment
 #version 330 core
+out vec4 color;
 
-layout (location = 0) out vec4 color;
 in vec3 ourColor;
+in vec2 ourTexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    color = vec4(ourColor, 1.0f);
+    color = mix(texture(texture1, ourTexCoord), texture(texture2, ourTexCoord), 0.2) * vec4(ourColor, 1.0f);
 }
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -3,9 +3,12 @@
 
 #include <iostream>
 // clang-format off
-#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#include <glm/vec3.hpp>
 // clang-format on
 
 constexpr int WIDTH = 800;
@@ -38,18 +41,18 @@ int main()
     glViewport(0, 0, WIDTH, HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // clang-format off
     constexpr float vertices[] = {
-        -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // Top left
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom left
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom Right
-        0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // Top Right
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // Top Center
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
     };
+    // clang-format on
 
     constexpr unsigned int indices[] = {
-        4, 1, 2, // Triangle
-        0, 1, 2, // Square Left
-        0, 2, 3 // Square Right
+        3, 2, 1,
+        3, 1, 0
     };
 
     // Buffers
@@ -69,19 +72,68 @@ int main()
 
     // 1. then set the vertex attributes pointers
     // Positions
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(0));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(0));
     glEnableVertexAttribArray(0);
 
     // Colors
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    // Texture Coordinates
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // uncomment this call to draw in wireframe polygons.
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    // Textures
+    stbi_set_flip_vertically_on_load(true);
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+
+    int width, height, nrChannels;
+    void* data = stbi_load("resources/textures/container.jpg", &width, &height, &nrChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cerr << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+
+    data = stbi_load("resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cerr << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     const mc::Shader basicShader("resources/shaders/basic.glsl");
+    basicShader.use();
+    basicShader.setInt("texture1", 0);
+    basicShader.setInt("texture2", 1);
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
@@ -89,9 +141,16 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Start Rendering */
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
         basicShader.use();
+
         glBindVertexArray(vertexArrayObject);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, reinterpret_cast<void*>(0));
         /* Stop Rendering */
 
         glfwSwapBuffers(window);
